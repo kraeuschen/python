0cdb9d6fc1f2ee61745f04833c648a92fd384e3e
Jan Krause <jan.krause@wywy.com>
2013-10-20 13:36:29 +0200

    more complicated
    still not a valid solution

--- srv/eight_queens/solve.py
+++ srv/eight_queens/solve.py
@@ -2,13 +2,38 @@ def solve_queens():
     rows = 8
     columns = 8
     solutions = []
+    failes = []
 
-    for row in range(rows):
-        for column in range(columns):
-            if no_conflict(row, column, solutions):
-                solutions.append({'row': row, 'column': column})
+    start_row = 0
+    start_column = 0
+
+    while (start_column < columns):
+        for row in range(rows):
+            for column in range(columns):
+                if not has_failed(row, column, failes) and no_conflict(row, column, solutions):
+                    solutions.append({'row': row, 'column': column})
+
+
+        if len(solutions) < rows:
+            solutions = []
+            failes.append({'row': start_row, 'column': start_column})
+            start_row += 1
+
+            if (start_row == rows):
+                start_row = 0
+                start_column += 1
+        else:
+            break
 
     return solutions
+
+
+def has_failed(row, column, failes):
+    for fail in failes:
+        if fail['row'] == row and fail['column'] == column:
+            return True
+
+    return False
 
 
 def no_conflict(row, column, solutions):
@@ -26,7 +51,7 @@ def no_conflict(row, column, solutions):
             #print 'next'
             return False
 
-        if (solution['column'] - 1) == column  and (solution['row'] + 1 == row):
+        if (solution['column'] - 1) == column and (solution['row'] + 1 == row):
             #print 'prev'
             return False
 
