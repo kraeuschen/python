3887999850fdb4ad390bb7d5db505a23b3ecfd36
Jan Krause <jan.krause@wywy.com>
2013-10-20 11:39:15 +0200

    first step of implementation

--- srv/eight_queens/solve.py
+++ srv/eight_queens/solve.py
@@ -1,7 +1,19 @@
 
 def solve_queens():
+    rows = 8
+    columns = 8
     solutions = []
+
+    for row in range(rows):
+        for column in range(columns):
+            if no_conflict(row, column, solutions):
+                solutions.append({'row': row, 'column': column})
+
     return solutions
+
+def no_conflict(row, colum, solutions):
+    return True
+
 
 for solution in solve_queens():
     print solution
--- srv/eight_queens/tests/test_solve.py
+++ srv/eight_queens/tests/test_solve.py
@@ -2,13 +2,21 @@ __author__ = 'jankrause'
 
 import unittest2
 
-from solve import solve_queens
+from solve import solve_queens, no_conflict
 
 
 class TestSolve(unittest2.TestCase):
     def test_solve_queens(self):
         expected = []
+
+        for row in range(8):
+            for column in range(8):
+                expected.append({'row': row, 'column': column})
+
         self.assertEqual(solve_queens(), expected)
+
+    def test_no_conflict(self):
+        self.assertTrue(no_conflict(1,2,[]))
 
 
 if __name__ == '__main__':
