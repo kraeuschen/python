2afcff7ea07c90f0d4fb2bbc2f62125524f93f40
Jan Krause <jan.krause@wywy.com>
2013-11-09 18:15:06 +0100

    puzzle solved
    iterative version

--- srv/eight_queens/solve.py
+++ srv/eight_queens/solve.py
@@ -1,82 +1,39 @@
 def solve_queens():
-    rows = 8
-    columns = 8
+    board = 8
     solutions = []
-    failes = []
 
-    start_row = 0
-    start_column = 0
-
-    while (start_column < columns):
-        for row in range(rows):
-            for column in range(columns):
-                if not has_failed(row, column, failes) and no_conflict(row, column, solutions):
-                    solutions.append({'row': row, 'column': column})
-
-        if len(solutions) < rows:
-            print solutions
-            solutions = []
-            failes.append({'row': start_row, 'column': start_column})
-            start_row += 1
-
-            if (start_row == rows):
-                start_row = 0
-                start_column += 1
-        else:
-            break
+    for row in range(board):
+        solutions = get_solution(row, board, solutions)
 
     return solutions
 
+def get_solution(row, columns, solutions):
+    result = []
 
-def has_failed(row, column, failes):
-    for fail in failes:
-        if fail['row'] == row and fail['column'] == column:
-            return True
+    for column in range(columns):
+        if not len(solutions):
+            result.append([(row, column)])
+        else:
+            for solution in solutions:
+                if no_conflict(row, column, solution):
+                    result.append(solution + [(row, column)])
 
-    return False
-
+    return result
 
 def no_conflict(row, column, solutions):
     for solution in solutions:
-
-        # horizontal
-        if solution['row'] == row:
+        r,c = solution
+        if r == row:
             return False
 
-        # vertical
-        if solution['column'] == column:
+        if c == column:
             return False
 
-        right = solution['column']
-        left = solution['column']
-        down = solution['row']
-        up = solution['row']
-        offset = 1
+        if (column - c) == (row - r):
+            return False
 
-        # diagonal
-        while offset < 8:
-            right += 1
-            left -= 1
-            down += 1
-            up -= 1
-
-            if (right == column and down == row):
-                #print 'next'
-                return False
-
-            if (left == column and down == row):
-                #print 'prev'
-                return False
-
-            if (right == column and up == row):
-                #print 'next'
-                return False
-
-            if (left == column and up == row):
-                #print 'prev'
-                return False
-
-            offset += 1
+        if (column -c) == -(row -r):
+            return False
 
     return True
 
