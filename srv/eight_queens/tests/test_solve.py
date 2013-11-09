c453844d03e0f217473cdf19b877fb96d81d0760
Jan Krause <jan.krause@wywy.com>
2013-11-09 18:36:32 +0100

    fix unittest
    blame me, it was not testdriven :P

--- srv/eight_queens/tests/test_solve.py
+++ srv/eight_queens/tests/test_solve.py
@@ -9,27 +9,20 @@ class TestSolve(unittest2.TestCase):
     def test_no_conflict(self):
         self.assertTrue(no_conflict(1,2,[]))
 
-        solutions = []
-        solutions.append({'row': 0, 'column': 0});
+        solutions = [(0, 0)];
 
         self.assertFalse(no_conflict(0,4, solutions))
         self.assertTrue(no_conflict(1,3, solutions))
 
-        solutions = []
-        solutions.append({'column': 0, 'row': 0})
-        solutions.append({'column': 2, 'row': 1})
-        solutions.append({'column': 4, 'row': 2})
-        solutions.append({'column': 6, 'row': 3})
+        solutions = [(0,0),(1,2),(2,4),(3,6)]
 
         self.assertTrue(no_conflict(4, 1, solutions))
 
-        solutions = []
-        solutions.append({'column': 0, 'row': 0})
+        solutions = [(0, 0)]
 
         self.assertFalse(no_conflict(7, 7, solutions))
 
-        solutions = []
-        solutions.append({'column': 7, 'row': 7})
+        solutions = [(7, 7)]
 
         self.assertFalse(no_conflict(0, 0, solutions))
 
